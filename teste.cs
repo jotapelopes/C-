8667a8736b7107793d3b165ca9f5097cecd25cd4
João Pedro <127902932+jotapelopes@users.noreply.github.com>
2024-02-19 21:32:09 -0300

    Update teste.cs

--- teste.cs
+++ teste.cs
@@ -1,8 +1,14 @@
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Console.ReadKey();
+
+// Exercício 1:
+
 namespace ConsoleApp2
 {
     internal class Program
     {
-        static double Main(string[] args, double? raio)
+        public static void Main(string[] args)
         {
             //Console.WriteLine("Digite o primeiro lado do retângulo: ");
             //double? var1 = Convert.ToDouble(Console.ReadLine());
@@ -22,15 +28,19 @@ namespace ConsoleApp2
 
             //Console.WriteLine("O número elevado ao cubo é: " + elevCubo);
 
-            Console.WriteLine("Faça um programa para calcular a area de qualquer circunferência");
+            //Console.WriteLine("Faça um programa para calcular a area de qualquer circunferência");
 
-            Console.WriteLine("Digite o raio da circunferência em questão");
-            double? raio1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Digite o raio da circunferência em questão");
+            //double? raio1 = Convert.ToDouble(Console.ReadLine());
 
-            double? areaCircunf = 3.14 * Math.Pow((double)raio1, 2);
+            //double? areaCircunf = 3.14 * Math.Pow((double)raio1, 2);
 
-            Console.WriteLine("A área da circunferência é: " + areaCircunf);
+            //Console.WriteLine("A área da circunferência é: " + areaCircunf);
 
+            double far = 32;
+            double celsius = (5 * far - 160) / 9;
+
+            Console.WriteLine("A conversão é de: " + celsius);
         }
     }
 }
