f210f09452a69d06e395315f1fc743b4b75945ba
João Pedro <127902932+jotapelopes@users.noreply.github.com>
2024-02-21 22:03:30 -0300

    Create Windowsforms.cs

---
+++ Windowsforms.cs
@@ -0,0 +1,38 @@
+namespace lopes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            double l1 = Convert.ToDouble(textBox1.Text);
+            double l2 = Convert.ToDouble(textBox2.Text);
+            double area = l1 * l2;
+            label4.Text = area.ToString();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
