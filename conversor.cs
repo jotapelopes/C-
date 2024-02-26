fc4f6d5e4b1ea0ab058ed8386628b7f36f5211d9
João Pedro <127902932+jotapelopes@users.noreply.github.com>
2024-02-26 20:55:46 -0300

    Create conversor.cs

---
+++ conversor.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double hoje = Convert.ToDouble(textBox1.Text);
+            double ontem = Convert.ToDouble(textBox2.Text);
+
+            if (hoje <= ontem * 0.95)
+            {
+                Console.WriteLine("Comprar dolár");
+            }
+            else
+            {
+                string retorno = ("Não vale a pena");
+                label4.Text = retorno.ToString();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
